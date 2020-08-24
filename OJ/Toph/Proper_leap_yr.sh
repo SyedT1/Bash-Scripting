45c0379df8b079b0a2948846f4d73b1ca4ee61c6
Syed Tahsin <syedmuhaimintahsin@gmail.com>
2020-08-24 10:41:32 +0600

    Proper leap year

--- OJ/Toph/Proper_leap_yr.sh
+++ OJ/Toph/Proper_leap_yr.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 read n
-if (( n%400==0 ));then
-echo "Yes"
-elif (( n%4==0 && n%100!=0 ));then
-echo "Yes"
-else
-echo "No"
+if (( n%400==0 ));then echo "Yes"
+elif (( n%4==0 && n%100!=0 ));then echo "Yes"
+else echo "No"
 fi
