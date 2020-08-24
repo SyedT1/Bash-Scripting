0222a91cebf8c559ad26ff0e354e8dea26fc12cf
Syed Tahsin <syedmuhaimintahsin@gmail.com>
2020-08-24 13:18:43 +0600

    Print Triangle

--- Introductory/For Loops/Print_triangle.sh
+++ Introductory/For Loops/Print_triangle.sh
@@ -2,13 +2,9 @@
 read -p "Enter an input = " input
 for (( i=1; i <= input; i++ ));
 do
-  for (( k=1; k<=input-i;k++ ));
-  do
-    printf ' '
+  for (( k=1; k<=input-i;k++ )); do printf ' '
   done
-  for (( t=1; t<=i ; t++ ));
-  do
-    printf "* "
+  for (( t=1; t<=i ; t++ )); do printf "* "
   done
   printf '\n'
 done
