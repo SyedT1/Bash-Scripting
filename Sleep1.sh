9854a3132a51bdb3916e81ac0f27f27943264e10
Syed Tahsin <syedmuhaimintahsin@gmail.com>
2020-08-24 12:33:59 +0600

    Sleep Function part 1

--- Sleep1.sh
+++ Sleep1.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+read n
 s=0.1
-p=0.0
-for (( i=1; i<10; i++ ));do
- p=$(awk '{print $1+$2}' <<< "${s} ${p}" )
- echo "$p"
- sleep $p
+p=0
+for (( i=1; i<=n; i++ ));do
+p=$(awk '{print $1+$2}' <<< "${s} ${p}" )
+echo "$p"
+sleep $p
 done
